6dd53a582b9cb3f820d71d9626e8474fbadb8584
remis08 <shimbo.remi08@gmail.com>
2024-08-16 18:27:23 +0900

    furnitureストアのアップロード

---
+++ furniturestore-site/js/script.js
@@ -0,0 +1,25 @@
+//西暦の取得
+
+//ハンバーガーボタン
+//#ham-btnをon.clickしたら
+$('#ham-btn').on('click', function () {
+    $(this).toggleClass('is-active');
+
+    $('#sp-nav').toggleClass('slide')
+});
+
+
+//スクロールイベント
+$(window).on('scroll', function () {
+    //自身のスクロール量を取得し代入
+    let y = $(window).scrollTop();
+    console.log(y);
+    //headerの高さを取得
+    let headerH = $('header').outerHeight();
+    console.log(headerH);
+
+    if (y >= headerH) {
+        $('body').addClass('scroll');
+    } else {
+        $('body').removeClass('scroll');
+    }});
